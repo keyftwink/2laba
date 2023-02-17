ede7eb04197c8fddff98da8e963553be0b2e8cef
keyftwink <mailfordiscord2020@mail.ru>
2023-02-17 20:11:50 +0300

    Добавьте файлы проекта.

---
+++ 2laba/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Введите размерность массива: ");
+        int lenght = Convert.ToInt32(Console.ReadLine());
+
+        Random random = new Random();
+        int[] array = new int[lenght];
+
+        Console.WriteLine("Сгенерированный массив: ");
+        for(int i = 0; i < lenght; i++)
+        {
+            array[i] = random.Next(-20, 20);
+            Console.Write(array[i] + ", ");
+        }
+
+        for(int i = 1; i < lenght; i++)
+        {
+            int k = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && k < array[j])
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = k;
+        }
+
+        Console.WriteLine("\nОтсортированный массив: ");
+        for (int i = 0; i < lenght; i++)
+        {
+            Console.Write(array[i] + ", ");
+        }
+    }
+}
+
+
+        
